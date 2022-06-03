e1ec37d1177753b76a165ac578d9c5c6ebcd7318
Cristhian Morales <cristhian@denkitronik.com>
2022-06-03 14:48:16 -0500

    SGDP-54 Header y footer en proceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
