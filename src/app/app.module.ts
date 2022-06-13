975f1fbd478e09903eaeef68a96590b9f4b3f576
Cristhian Morales <cristhian@denkitronik.com>
2022-06-13 09:04:11 -0500

    SGDP-54 Header,footer y contacto completos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
 import { HeaderComponent } from './header/header.component';
-
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const rutas: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: 'contacto', component: ContactComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -16,7 +22,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas),
   ],
   providers: [],
   bootstrap: [AppComponent]
